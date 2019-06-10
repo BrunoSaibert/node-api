85508818ff36a0aee0d15572634e94f2b3ae9719
BrunoSaibert <brunosaibert@gmail.com>
2019-06-10 17:07:34 -0300

    Adicionar arquivos

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const requireDir = require('require-dir');
+
+// Iniciando o App
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+//Inniciando o DB
+mongoose.connect(
+  'mongodb://192.168.99.100:27017/nodeapi',
+  { useNewUrlParser: true }
+);
+requireDir('./src/models/');
+
+// Rotas
+app.use('/api', require('./src/routes'));
+
+app.listen(3001);
